3e4893b3b992e4482876a65dcf4580d41ee0b667
charlygrahy23 <dipta.biswas231998@gmail.com>
2022-10-09 20:46:43 +0530

    update base url

--- _http/index.ts
+++ _http/index.ts
@@ -3,7 +3,7 @@ import { toast } from 'components/alert';
 import { useRouter } from 'next/router';
 
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: `${process.env.BASE_URL}/api`,
 });
 
 axiosInstance.interceptors.response.use(
